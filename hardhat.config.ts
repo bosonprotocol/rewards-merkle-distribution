b5525e4ae470378eb96ba30e114ac9f443b4b2c0
levalleux-ludo <levalleux_ludo@hotmail.com>
2021-09-16 17:25:14 +0200

    add deploy task in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,10 @@ import "@nomiclabs/hardhat-waffle";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
+const lazyImport = async (module: any) => {
+	return await import(module);
+}
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -16,6 +20,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+task("deploy", "Deploy contracts on a provided network")
+	.addOptionalParam("env", "Which environment is going to be used for contract deployment. Choose between prod, demo, dev or empty for local deployment", "hardhat")
+	.setAction( async ({env}) => {
+		const { deploy } = await lazyImport('./scripts/deploy')
+		await deploy(env);
+	})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,7 +8,7 @@ import * as hre from "hardhat";
 const ZERO_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
 const TOKEN_ADDRESS = '0x976EA74026E726554dB657fA54763abd0C3a0aa9';
 
-async function main() {
+export async function deploy() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
   //
@@ -19,17 +19,22 @@ async function main() {
   // We get the contract to deploy
   const MerkleDistributor = await hre.ethers.getContractFactory("MerkleDistributor");
   const merkleDistributor = await MerkleDistributor.deploy(TOKEN_ADDRESS, ZERO_BYTES32);
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log('network', hre.network.name, hre.network.config);
+  console.log('deployer', await deployer.getAddress());
 
   await merkleDistributor.deployed();
 
   console.log("MerkleDistributor deployed to:", merkleDistributor.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// deploy()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
+
